95130e1893ae4e982baca4768a5340f400e642ca
charanjithreddy <charanjith.angadi@gmail.com>
2025-10-24 11:14:34 +0530

    Extracted frames from jumbled_video.mp4 to extracted_frames using video_to_frmaes.py

---
+++ video_to_frames.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+
+video_path = "C:\\Users\\lenovo\\OneDrive\\Desktop\\JUMBLED FRAMES RECONSTRUCTION\\jumbled_video.mp4"
+output_directory = "extracted_frames"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+cap = cv2.VideoCapture(video_path)
+
+frame_count = 0
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Construct the filename for the current frame
+    frame_filename = os.path.join(output_directory, f"frame_{frame_count:04d}.jpg")
+
+    # Save the frame as an image
+    cv2.imwrite(frame_filename, frame)
+
+    print(f"Frame {frame_count} saved as {frame_filename}")
+    frame_count += 1
+
+cap.release()
+cv2.destroyAllWindows()  # Close any OpenCV windows if opened
+print(f"Successfully extracted {frame_count} frames to {output_directory}")
